e35ae7fecc146305ea6df1ada217d250d5b061da
Mitemi <miter360@gmail.com>
2025-06-20 21:14:23 -0400

    Config

---
+++ kubejs/client_scripts/itemNames.js
@@ -0,0 +1,8 @@
+ClientEvents.lang('en_us', event => {
+  event.renameItem('quarryplus:quarry', 'Quarry');
+  event.renameItem('quarryplus:workbench', 'Integral Workbench');
+  event.renameItem('quarryplus:mining_well', 'Mining Well');
+  event.renameItem('quarryplus:pump_plus', 'Pump');
+  event.renameItem('quarryplus:marker', 'Marker');
+  event.renameItem('ftbquests:book', 'Eleonore\'s Travels');
+})
---
+++ kubejs/startup_scripts/displayNames.js
@@ -0,0 +1,8 @@
+Platform.mods.quarryplus.name = 'Quarry+';
+Platform.mods.hpm.name = 'Swashbucklers';
+Platform.mods.asr.name = 'Asgard Shield';
+Platform.mods.integrateddynamics.name = 'Integrated Dynamics';
+Platform.mods.integratedtunnels.name = 'Integrated Tunnels';
+Platform.mods.integratedterminals.name = 'Integrated Terminals';
+Platform.mods.oreberriesreplanted.name = 'Oreberries';
+Platform.mods.ftbquests.name = 'Eleonore\'s Travels';
