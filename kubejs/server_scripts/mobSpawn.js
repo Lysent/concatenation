f0415c29d8f3f6c5ce970c9b74ac30e03831a860
Mitemi <miter360@gmail.com>
2026-02-09 22:15:19 -0500

    1.4.5 Part 2

--- kubejs/server_scripts/mobSpawn.js
+++ kubejs/server_scripts/mobSpawn.js
@@ -1,17 +1,30 @@
-ServerEvents.highPriorityData(event => {
-  event.addJson(
-    'kubejs:forge/biome_modifier/air_mage_spawn.json',
-    {
-      "type": "forge:add_spawns",
-      "biomes": "minecraft:is_overworld",
-      "spawners": [
-        {
-          "type": "ars_elemental:air_mage",
-          "weight": 500,   // TEST VALUE (extremely common)
-          "minCount": 1,
-          "maxCount": 3
-        }
-      ]
+EntityEvents.spawned(event => {
+  if (event.level.isClientSide()) return
+  if (event.entity.type !== 'minecraft:zombie') return
+
+  event.server.scheduleInTicks(1, () => {
+    if (!event.entity.isAlive()) return
+
+    if (Math.random() > 0.03) return
+
+    const pos = event.entity.blockPosition()
+    event.entity.discard()
+
+    const mages = [
+      'ars_elemental:air_mage',
+      'ars_elemental:earth_mage',
+      'ars_elemental:fire_mage',
+      'ars_elemental:water_mage'
+    ]
+    const chosenMage = mages[Math.floor(Math.random() * mages.length)]
+
+    const mageEntity = event.level.createEntity(chosenMage)
+    if (!mageEntity) {
+      console.warn(`Failed to create entity: ${chosenMage}`)
+      return
     }
-  )
+
+    mageEntity.setPos(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5)
+    event.level.addFreshEntity(mageEntity)
+  })
 })
