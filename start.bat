48bf25c8a1dd15caa0251957cf96a0ebc8fac4a4
Mitemi <miter360@gmail.com>
2026-04-10 20:35:49 -0400

    update to .10 forge

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.0/win_args.txt
+java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.20.1-47.4.10/unix_args.txt
 pause
